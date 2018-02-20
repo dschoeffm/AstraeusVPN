441d63372bd20f37f5cf804104afb6e894f250e0
Dominik Schoeffmann <schoeffm@in.tum.de>
2018-02-20 18:40:12 +0100

    start implementing a custom protocol

---
+++ include/cryptoProto.hpp
@@ -0,0 +1,78 @@
+#ifndef CRYPTOPROTO_HPP
+#define CRYPTOPROTO_HPP
+
+#include <cstdint>
+
+#include "sodium.h"
+
+namespace AstraeusProto {
+struct initMsg {
+	static constexpr uint8_t initMsgType = 0;
+
+	uint8_t type;
+	uint8_t ecdhe[crypto_kx_PUBLICKEYBYTES];
+	uint8_t ecdsa[crypto_sign_PUBLICKEYBYTES];
+	uint8_t nonce[16];
+} __attribute__((packed));
+
+struct authMsg {
+	static constexpr uint8_t authMsgType = 1;
+
+	uint8_t type;
+	uint8_t sig[crypto_sign_BYTES];
+} __attribute__((packed));
+
+struct tunnelMsg {
+	static constexpr uint8_t tunnelMsgType = 2;
+
+	uint8_t type;
+	uint8_t nonce[crypto_aead_chacha20poly1305_IETF_NPUBBYTES];
+	uint8_t mac[crypto_aead_chacha20poly1305_IETF_ABYTES];
+} __attribute__((packed));
+
+struct identityHandle {
+	uint8_t pubKey[crypto_sign_PUBLICKEYBYTES];
+	uint8_t secKey[crypto_sign_SECRETKEYBYTES];
+};
+
+struct protoHandle {
+	AstraeusProto::initMsg initiator;
+	AstraeusProto::initMsg responder;
+	uint8_t txKey[crypto_aead_chacha20poly1305_IETF_KEYBYTES];
+	uint8_t rxKey[crypto_aead_chacha20poly1305_IETF_KEYBYTES];
+	uint8_t txNonce[crypto_aead_chacha20poly1305_IETF_NPUBBYTES];
+	uint8_t rxNonce[crypto_aead_chacha20poly1305_IETF_NPUBBYTES];
+	uint8_t ecdhPub[crypto_kx_PUBLICKEYBYTES];
+	uint8_t ecdhSec[crypto_kx_SECRETKEYBYTES];
+	uint8_t peerEcdsaPub[crypto_sign_PUBLICKEYBYTES];
+	identityHandle *ident;
+
+	enum { INIT, AUTH, TUNNEL } type;
+};
+
+void fillInitMsg(struct initMsg *msg, protoHandle &handle);
+void handleInitMsg(struct initMsg *msg, protoHandle &handle);
+
+void fillAuthMsg(struct authMsg *msg, protoHandle &handle);
+void handleAuthMsg(struct authMsg *msg, protoHandle &handle);
+
+// Will return 0 if handshake is ongoing
+// Will return 1 if tunnel is ready
+int handleHandshakeServer(protoHandle &handle, uint8_t *msg, int &sendCount);
+int handleHandshakeClient(protoHandle &handle, uint8_t *msg, int &sendCount);
+
+// Generate the identity
+void generateIdentity(identityHandle &ident);
+
+// Generate Init message and protoHandle
+// Will write init msg into "msg", and handle into "handle"
+void generateInit(identityHandle &ident, protoHandle &handle, uint8_t *msg);
+
+void decryptTunnelMsg(uint8_t *msgIn, unsigned int msgInLen, uint8_t *msgOut,
+	unsigned int &msgOutLen, protoHandle &handle);
+void encryptTunnelMsg(uint8_t *msgIn, unsigned int msgInLen, uint8_t *msgOut,
+	unsigned int &msgOutLen, protoHandle &handle);
+
+}; // namespace AstraeusProto
+
+#endif /* CRYPTOPROTO_HPP */
---
+++ lib/cryptoProto.cpp
@@ -0,0 +1,206 @@
+#include <cassert>
+#include <cstring>
+#include <stdexcept>
+
+#include "cryptoProto.hpp"
+
+using namespace AstraeusProto;
+
+void AstraeusProto::fillInitMsg(struct initMsg *msg, protoHandle &handle) {
+	crypto_kx_keypair(handle.ecdhPub, handle.ecdhSec);
+
+	memcpy(msg->ecdhe, handle.ecdhPub, crypto_kx_PUBLICKEYBYTES);
+	randombytes_buf(msg->nonce, sizeof(msg->nonce));
+	memcpy(handle.txNonce, msg->nonce, sizeof(msg->nonce));
+	msg->type = AstraeusProto::initMsg::initMsgType;
+	memcpy(msg->ecdsa, handle.ident->pubKey, sizeof(msg->ecdsa));
+
+	handle.type = protoHandle::INIT;
+};
+
+void AstraeusProto::handleInitMsg(struct initMsg *msg, protoHandle &handle) {
+	if (handle.type != protoHandle::INIT) {
+		throw new std::runtime_error("handleInitMsg() current state is not init");
+	}
+
+	if (msg->type != initMsg::initMsgType) {
+		throw new std::runtime_error("handleInitMsg() msg is not init");
+	}
+
+	// Create the session keys
+	if (crypto_kx_client_session_keys(
+			handle.rxKey, handle.txKey, handle.ecdhPub, handle.ecdhSec, msg->ecdhe) != 0) {
+		throw new std::runtime_error("handleInitMsg() key exchange failed");
+	}
+
+	memcpy(handle.peerEcdsaPub, msg->ecdsa, sizeof(handle.peerEcdsaPub));
+	memcpy(handle.rxNonce, msg->nonce, sizeof(handle.rxNonce));
+
+	handle.type = protoHandle::AUTH;
+
+	/*
+	 * XXX At this point, a peer would think about accepting the ECDSA key
+	 * XXX This would be done against a database, or by similar means
+	 * XXX An attack on this implementation is obvious
+	 */
+};
+
+void AstraeusProto::fillAuthMsg(struct authMsg *msg, protoHandle &handle) {
+	if (handle.type != protoHandle::AUTH) {
+		throw new std::runtime_error("fillAuthMsg() current state is not auth");
+	}
+
+	msg->type = AstraeusProto::authMsg::authMsgType;
+
+	crypto_sign_state state;
+	crypto_sign_update(
+		&state, reinterpret_cast<uint8_t *>(&handle.initiator), sizeof(struct initMsg));
+	crypto_sign_update(
+		&state, reinterpret_cast<uint8_t *>(&handle.responder), sizeof(struct initMsg));
+
+	crypto_sign_final_create(&state, msg->sig, NULL, handle.ident->secKey);
+};
+
+void AstraeusProto::handleAuthMsg(struct authMsg *msg, protoHandle &handle) {
+	if (handle.type != protoHandle::AUTH) {
+		throw new std::runtime_error("handleAuthMsg() current state is not auth");
+	}
+
+	if (msg->type != authMsg::authMsgType) {
+		throw new std::runtime_error("handleAuthMsg() msg is not auth");
+	}
+
+	crypto_sign_state state;
+	crypto_sign_update(
+		&state, reinterpret_cast<uint8_t *>(&handle.initiator), sizeof(struct initMsg));
+	crypto_sign_update(
+		&state, reinterpret_cast<uint8_t *>(&handle.responder), sizeof(struct initMsg));
+
+	int ret = crypto_sign_final_verify(&state, msg->sig, handle.peerEcdsaPub);
+	if (ret != 0) {
+		throw new std::runtime_error(
+			"AstraeusProto::verifyAuthMsg() Signature doesn't match");
+	}
+
+	assert(crypto_aead_chacha20poly1305_IETF_KEYBYTES == crypto_kx_SESSIONKEYBYTES);
+
+	ret = crypto_kx_client_session_keys(
+		handle.rxKey, handle.txKey, handle.ecdhPub, handle.ecdhSec, handle.peerEcdsaPub);
+	if (ret != 0) {
+		throw new std::runtime_error("AstraeusProto::verifyAuthMsg() Key exchange failed");
+	}
+
+	memset(handle.rxNonce, 0, sizeof(handle.rxNonce));
+	memset(handle.txNonce, 0, sizeof(handle.txNonce));
+
+	handle.type = protoHandle::TUNNEL;
+};
+
+void AstraeusProto::decryptTunnelMsg(uint8_t *msgIn, unsigned int msgInLen, uint8_t *msgOut,
+	unsigned int &msgOutLen, protoHandle &handle) {
+	int ret;
+
+	tunnelMsg *header = reinterpret_cast<tunnelMsg *>(msgIn);
+
+	if (header->type != AstraeusProto::tunnelMsg::tunnelMsgType) {
+		throw new std::runtime_error("AstraeusProto::decryptTunnelMsg() Msg type is wrong");
+	}
+
+	if (sodium_compare(handle.rxNonce, header->nonce, sizeof(handle.rxNonce)) != -1) {
+		throw new std::runtime_error("AstraeusProto::decryptTunnelMsg() Unexpected nonce");
+	}
+
+	uint8_t *payload = msgIn + sizeof(tunnelMsg);
+	ret = crypto_aead_chacha20poly1305_ietf_decrypt_detached(msgOut, NULL, payload,
+		msgInLen - sizeof(struct tunnelMsg), header->mac, header->nonce,
+		sizeof(header->nonce), header->nonce, handle.rxKey);
+
+	msgOutLen = msgInLen - sizeof(struct tunnelMsg);
+	memcpy(handle.rxNonce, header->nonce, sizeof(handle.rxNonce));
+
+	if (ret != 0) {
+		throw new std::runtime_error("AstraeusProto::decryptTunnelMsg() decryption failed");
+	}
+};
+
+void AstraeusProto::encryptTunnelMsg(uint8_t *msgIn, unsigned int msgInLen, uint8_t *msgOut,
+	unsigned int &msgOutLen, protoHandle &handle) {
+	int ret;
+
+	tunnelMsg *header = reinterpret_cast<tunnelMsg *>(msgOut);
+
+	uint8_t *payload = msgOut + sizeof(tunnelMsg);
+	memcpy(header->nonce, handle.txNonce, sizeof(header->nonce));
+
+	ret = crypto_aead_chacha20poly1305_ietf_encrypt_detached(payload, header->mac, NULL,
+		msgIn, msgInLen, header->nonce, sizeof(header->nonce), NULL, header->nonce,
+		handle.txKey);
+
+	if (ret != 0) {
+		throw new std::runtime_error("AstraeusProto::encryptTunnelMsg() encryption failed");
+	}
+	msgOutLen = msgInLen + sizeof(tunnelMsg);
+
+	sodium_increment(handle.txNonce, sizeof(handle.txNonce));
+};
+
+int AstraeusProto::handleHandshakeServer(protoHandle &handle, uint8_t *msg, int &sendCount) {
+	switch (handle.type) {
+	case protoHandle::INIT:
+		handleInitMsg(reinterpret_cast<initMsg *>(msg), handle);
+		fillInitMsg(reinterpret_cast<initMsg *>(msg), handle);
+		sendCount = sizeof(initMsg);
+		break;
+
+	case protoHandle::AUTH:
+		handleAuthMsg(reinterpret_cast<authMsg *>(msg), handle);
+		fillAuthMsg(reinterpret_cast<authMsg *>(msg), handle);
+		sendCount = sizeof(authMsg);
+		break;
+
+	default:
+		throw new std::runtime_error("AstraeusProto::handleHandshake() type unknown");
+		break;
+	}
+
+	if (handle.type == protoHandle::TUNNEL) {
+		return 1;
+	} else {
+		return 0;
+	}
+};
+
+int AstraeusProto::handleHandshakeClient(protoHandle &handle, uint8_t *msg, int &sendCount) {
+	switch (handle.type) {
+	case protoHandle::INIT:
+		handleInitMsg(reinterpret_cast<initMsg *>(msg), handle);
+		fillAuthMsg(reinterpret_cast<authMsg *>(msg), handle);
+		sendCount = sizeof(authMsg);
+		break;
+
+	case protoHandle::AUTH:
+		handleAuthMsg(reinterpret_cast<authMsg *>(msg), handle);
+		sendCount = 0;
+		break;
+
+	default:
+		throw new std::runtime_error("AstraeusProto::handleHandshake() type unknown");
+		break;
+	}
+
+	if (handle.type == protoHandle::TUNNEL) {
+		return 1;
+	} else {
+		return 0;
+	}
+};
+
+void AstraeusProto::generateIdentity(identityHandle &ident) {
+	crypto_sign_keypair(ident.pubKey, ident.secKey);
+};
+
+void AstraeusProto::generateInit(identityHandle &ident, protoHandle &handle, uint8_t *msg) {
+	memset(&handle, 0, sizeof(protoHandle));
+	handle.ident = &ident;
+	fillInitMsg(reinterpret_cast<initMsg *>(msg), handle);
+};
