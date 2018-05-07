b63fac6637d4fb55c487036694a0a4a72aabd85c
Dominik Schoeffmann <schoeffm@in.tum.de>
2018-05-07 13:43:07 +0200

    add dtlsClientLoad.cpp

---
+++ src/dtlsClientLoad.cpp
@@ -0,0 +1,191 @@
+#include <array>
+#include <cerrno>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+#include <thread>
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <signal.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+#include "common.hpp"
+#include "dtls.hpp"
+#include "tap.hpp"
+
+using namespace std;
+
+// Taken from:
+// https://stackoverflow.com/a/20602159
+struct pairhash {
+public:
+	template <typename T, typename U> std::size_t operator()(const std::pair<T, U> &x) const {
+		return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+	}
+};
+
+static int stopFlag = 0;
+
+void sigHandler(int sig) {
+	(void)sig;
+	stopFlag = 1;
+}
+
+int handlePacket(int fd, DTLS::Connection &conn, char *buf, int bufLen, int recvBytes,
+	struct sockaddr_in *src_addr, uint8_t *wData, unsigned int wDataLen) {
+
+	DEBUG_ENABLED(std::cout << "run handlePacket" << std::endl;)
+
+	if (recvBytes > 0) {
+		if (BIO_write(conn.rbio, buf, recvBytes) != recvBytes) {
+			throw new std::runtime_error("handlePacket() BIO_write() failed");
+		}
+	}
+
+	if (SSL_is_init_finished(conn.ssl)) {
+		// Here the handshake is already finished
+
+		// Try to read incoming byte from the connection
+		int readLen = SSL_read(conn.ssl, buf, bufLen);
+		if ((readLen <= 0) && (SSL_get_shutdown(conn.ssl) == 0)) {
+			throw new std::runtime_error("handlePacket() SSL_read() failed");
+		}
+
+		// Throw the data away
+		(void)buf;
+	} else {
+		// We are currently conducting a handshake
+
+		std::cout << "handlePacket() running handshake" << std::endl;
+
+		// The return code is pretty useless, throw it away
+		SSL_connect(conn.ssl);
+
+		if (SSL_is_init_finished(conn.ssl)) {
+			std::cout << "handlePacket() Handshake finished" << std::endl;
+			std::cout << "Algorith: " << SSL_get_cipher_name(conn.ssl)
+					  << " Keylength: " << SSL_get_cipher_bits(conn.ssl, nullptr)
+					  << std::endl;
+			SSL_write(conn.ssl, wData, wDataLen);
+		}
+	}
+
+	// Write out all data
+	int readCount;
+	while ((readCount = BIO_read(conn.wbio, buf, bufLen)) > 0) {
+		int sendBytes = sendto(
+			fd, buf, readCount, 0, (struct sockaddr *)src_addr, sizeof(struct sockaddr_in));
+
+		if (sendBytes != readCount) {
+			throw new std::system_error(std::error_code(errno, std::generic_category()),
+				std::string("handlePacket() sendto() failed"));
+		} else {
+			DEBUG_ENABLED(std::cout << "handlePacket() Send packet to peer" << std::endl;)
+		}
+	}
+
+	if (SSL_get_shutdown(conn.ssl) != 0) {
+		if (SSL_shutdown(conn.ssl) == 1) {
+			SSL_free(conn.ssl);
+			return 1;
+		}
+	}
+
+	return 0;
+};
+
+void runThread(
+	struct sockaddr_in server, SSL_CTX *ctx, uint8_t *wData, unsigned int wDataLen) {
+
+	int ret;
+
+	int fd = DTLS::createSocket();
+	connect(fd, reinterpret_cast<sockaddr *>(&server), sizeof(struct sockaddr_in));
+
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
+	setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(struct timeval));
+
+	DTLS::Connection conn = DTLS::createClientConn(ctx);
+
+	char buf[2048];
+
+	// Start the handshake
+	SSL_connect(conn.ssl);
+	handlePacket(fd, conn, buf, 2048, 0, &server, wData, wDataLen);
+
+	while (stopFlag == 0) {
+		struct sockaddr_in src_addr;
+		socklen_t addrlen = sizeof(struct sockaddr_in);
+
+		// A DTLS packet is ready to be received
+		ret = recvfrom(fd, (void *)buf, 2048, 0, (struct sockaddr *)&src_addr, &addrlen);
+		if (ret < 0) {
+			if ((errno == EAGAIN) || (errno == EWOULDBLOCK) || (errno == EINTR)) {
+				continue;
+			} else {
+				throw new std::system_error(std::error_code(errno, std::generic_category()),
+					std::string("main() recvfrom() failed"));
+			}
+		} else {
+			DEBUG_ENABLED(std::cout << "received a packet" << std::endl;)
+		}
+
+		if (handlePacket(fd, conn, buf, 2048, ret, &src_addr, wData, wDataLen) == 1) {
+			stopFlag = 1;
+		}
+	}
+
+	close(fd);
+}
+
+void usage(string name) {
+	cout << "Usage: " << name << " <server IP> <server port> <numConns> <DataLen>" << endl;
+	exit(0);
+}
+
+int main(int argc, char **argv) {
+	try {
+		if (argc < 5) {
+			usage(string(argv[0]));
+		}
+
+		int numConns = atoi(argv[3]);
+		int DataLen = atoi(argv[4]);
+
+		// Prepare server address
+		struct sockaddr_in server;
+		if (inet_aton(argv[1], &server.sin_addr) != 1) {
+			cout << "inet_aton() failed" << endl;
+			exit(EXIT_FAILURE);
+		}
+		server.sin_port = htons(atoi(argv[2]));
+		server.sin_family = AF_INET;
+
+		signal(SIGINT, sigHandler);
+
+		SSL_CTX *ctx = DTLS::createClientCTX();
+
+		uint8_t *wData = reinterpret_cast<uint8_t *>(malloc(DataLen));
+
+		std::thread *threads =
+			reinterpret_cast<std::thread *>(malloc(sizeof(std::thread) * numConns));
+
+		for (int i = 0; i < numConns; i++) {
+			threads[i] = std::thread(runThread, server, ctx, wData, DataLen);
+			server.sin_port = htons(ntohs(server.sin_port) + 1);
+		}
+
+		std::cout << "Client shutting down..." << std::endl;
+	} catch (std::exception *e) {
+		std::cout << "Caught exception:" << std::endl;
+		std::cout << e->what() << std::endl;
+	}
+}
